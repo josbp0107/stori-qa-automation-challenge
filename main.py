eeb079b968e24451457f0bf70726817a4d75e200
Jose David Ballesteros Paternina <josebp0107@gmail.com>
2023-05-22 13:01:10 -0500

    Remove import

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from selenium.common import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from xmlrunner import xmlrunner
 
 
 class HomePageTests(unittest.TestCase):
